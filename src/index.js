cb9f2f99e2e74287470009a21a0fd8a3023eb3e1
shj3497 <shj3497@gmail.com>
2021-05-06 22:41:48 +0900

    final touch

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import SimpleHabit from './components/simpleHabit'
 // React.StrictMode : 에러 메세지 표시도구정도의 느낌
 ReactDOM.render(
   <React.StrictMode>
-    <App />,
-    <SimpleHabit />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
